1b38d47752618cef60f9a3ddfa0b3199dc2bdffb
ruz76 <ruz76@pcj424a.msad.vsb.cz>
2016-09-21 15:29:18 +0200

    initial commit

---
+++ src/main/java/cz/vsb/p4/testapp/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.vsb.p4.testapp;
+
+/**
+ *
+ * @author ruz76
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
