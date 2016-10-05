dad88fe434fc3404843a79a7065ed31c1825b8b4
Tomas Rehak <trehak1@gmail.com>
2016-10-05 16:23:18 +0200

    wip

--- src/main/java/cz/vsb/p4/testapp/Main.java
+++ src/main/java/cz/vsb/p4/testapp/Main.java
@@ -5,14 +5,24 @@
  */
 package cz.vsb.p4.testapp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 /**
- *
  * @author ruz76
  */
-public class Main {
-    
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+@SpringBootApplication(scanBasePackages = {"cz.vsb"})
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
     }
-    
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
 }
