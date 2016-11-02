13d353fd8686823389ff50192bd1a21ec4ac9529
ruz76 <jan.ruzicka@vsb.cz>
2016-11-02 15:51:01 +0100

    strom

---
+++ src/main/java/cz/vsb/p4/testapp/StromRepository.java
@@ -0,0 +1,28 @@
+package cz.vsb.p4.testapp;
+
+import com.google.common.collect.Maps;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * Created by ruz76 on 2.11.2016.
+ */
+@Service
+public class StromRepository {
+
+    private final Map<String, Strom> stromy = Maps.newHashMap();
+
+    public Strom getStrom(String nazev) {
+        return stromy.get(nazev);
+    }
+
+    public void deleteStrom(String nazev) {
+        stromy.remove(nazev);
+    }
+
+    public void saveStrom(Strom strom) {
+        stromy.put(strom.getNazev(), strom);
+    }
+
+}
