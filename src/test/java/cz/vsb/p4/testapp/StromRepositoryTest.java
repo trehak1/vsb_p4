9e53b19846958dd71be6378d083c4701c400566a
ruz76 <jan.ruzicka@vsb.cz>
2016-11-02 16:19:16 +0100

    strom

--- src/test/java/cz/vsb/p4/testapp/StromRepositoryTest.java
+++ src/test/java/cz/vsb/p4/testapp/StromRepositoryTest.java
@@ -43,13 +43,15 @@ public class StromRepositoryTest {
         repository.saveStrom(STROM);
         Strom retrieved = repository.getStrom(NAZEV);
         Assert.assertTrue(retrieved.vratFaze().isEmpty());
+        Assert.assertEquals(STROM, retrieved);
 
         Strom strom2 = new Strom(NAZEV);
         strom2.pridejFazi(1, Faze.RASENI);
+        repository.saveStrom(strom2);
         Strom retrieved2 = repository.getStrom(NAZEV);
 
-        Assert.assertFalse(retrieved.vratFaze().isEmpty());
-        Assert.assertTrue(1L == retrieved2.dejCasFaze(Faze.RASENI));
+        Assert.assertFalse(retrieved2.vratFaze().isEmpty());
+        Assert.assertTrue(1 == retrieved2.dejCasFaze(Faze.RASENI));
         Assert.assertEquals(strom2, retrieved2);
     }
 
